0cb7d04fc69a323e521508793c311534e2ae0f05
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-12-25 00:53:34 +0400

    implement env

---
+++ env.ts
@@ -0,0 +1,52 @@
+import { env as loadEnv } from "custom-env";
+import { z } from "zod";
+
+process.env.APP_STAGE = process.env.APP_STAGE || "dev";
+
+const isProduction = process.env.APP_STAGE === "production";
+const isDevelopment = process.env.APP_STAGE === "development";
+const isTesting = process.env.APP_STAGE === "test";
+
+if (isDevelopment) {
+  loadEnv();
+} else if (isTesting) {
+  loadEnv("test");
+}
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  APP_STAGE: z.enum(["dev", "production", "test"]).default("dev"),
+  PORT: z.coerce.number().positive().default(3001),
+  // DATABASE_URL: z.string().startsWith("postgresql://"),
+  // JWT_SECRET: z.string().min(32, "Must be at least 32 characters long"),
+  // JWT_EXPIRES_IN: z.string().default("7d"),
+  // BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
+});
+
+export type Env = z.infer<typeof envSchema>;
+let env: Env;
+
+try {
+  env = envSchema.parse(process.env);
+} catch (e) {
+  if (e instanceof z.ZodError) {
+    console.error("Invalid environment variables:", e.message);
+    console.error(JSON.stringify(z.flattenError(e).fieldErrors, null, 2));
+    e.issues.forEach((issue) => {
+      console.error(`${issue.path.join(".")}: ${issue.message}`);
+    });
+
+    process.exit(1);
+  }
+
+  throw e;
+}
+
+export const isProd = () => env.APP_STAGE === "production";
+export const isDev = () => env.APP_STAGE === "dev";
+export const isTest = () => env.APP_STAGE === "test";
+
+export { env };
+export default env;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { app } from "./server.ts";
+import { env } from "../env.ts";
 
-app.listen(3001, () => {
-  console.log("Server is running on port http://localhost:3001");
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port http://localhost:${env.PORT}`);
   console.log("Press Ctrl+C to stop the server");
-  console.log("Open your browser and navigate to http://localhost:3001/health");
+  console.log(
+    `Open your browser and navigate to http://localhost:${env.PORT}/health`
+  );
+  console.log(`Environment: ${env.APP_STAGE}`);
+  console.log(`Node Environment: ${env.NODE_ENV}`);
+  console.log(`Port: ${env.PORT}`);
 });
